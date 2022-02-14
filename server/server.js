c140424b57c0652e800bbaa46864814c52181020
maplesyrupman <maplesyrupman@protonmail.com>
2022-02-13 21:55:07 -0500

    prep for first deployment, start script in main package.json

--- server/server.js
+++ server/server.js
@@ -27,11 +27,11 @@ const startServer = async () => {
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, '/../client/build')))
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'/../client/build/index.html'))
-// })
+app.use(express.static(path.join(__dirname, '/../client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../client/build/index.html'))
+})
 
 db.once('open', () => {
     app.listen(PORT, () => {
